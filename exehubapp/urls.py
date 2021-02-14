50a9f4c6c5dd4e1e739f8240194b9ac4ee1f5086
Jack Gibbs <jackgibbs66@icloud.com>
2021-02-14 11:43:11 +0000

    Added two new templates, addevent.html and events.html. Have also added the paths to the urls.py file and updated views.py to reflect these new templates so that they display on the webpage.

--- exehubapp/urls.py
+++ exehubapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addevent/', views.addEvent, name='addevent'),
+    path('events/', views.viewEvents, name='addevent'),
 
 ]
--- exehubapp/views.py
+++ exehubapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+@csrf_exempt
+def addEvent(request):
+    return render(request, 'addevent.html')
+
+def viewEvents(request):
+    return render(request, 'events.html')
