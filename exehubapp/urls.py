a9a47210139c870eeecf63d5e0257b435076e99e
TravBird <tbird0055@gmail.com>
2021-02-15 18:49:56 +0000

    Adding basic Navbar, fixing logo bug, fixing a couple of mistakes. Adding NavBar for existing three pages. Fixing bug regarding logo not showing on most pages. Removing unnecessary script closing tag on events html file. Changing incorrect name of events path in exehubapp views.

--- exehubapp/urls.py
+++ exehubapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('addevent/', views.addEvent, name='addevent'),
-    path('events/', views.viewEvents, name='addevent'),
+    path('events/', views.viewEvents, name='events'),
 
 ]
