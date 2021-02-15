39a9ff4bbb8be50c8eb434de75221e079ff7ba36
James Moses <laffs.comic@gmail.com>
2021-02-15 23:17:50 +0000

    All tables added to the database

--- exehubapp/admin.py
+++ exehubapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from .models import Event
+from .models import Group
+from .models import User
+from .models import Attendee
+from .models import Member
 admin.site.register(Event)
+admin.site.register(Group)
+admin.site.register(User)
+admin.site.register(Attendee)
+admin.site.register(Member)
--- exehubapp/models.py
+++ exehubapp/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    group_name = models.CharField(max_length = 45)
+    group_owner = models.CharField(max_length = 45)
+    group_email = models.CharField(max_length = 45)
+    group_irc = models.CharField(max_length = 45)
+    fee = models.FloatField()
 class Event(models.Model):
     name = models.CharField(max_length = 45)
     owner = models.CharField(max_length = 45)
-    #group_id = models.ForeignKey(Groups)
-    set_date_time = models.DateField()
-    end_date_time = models.DateField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    set_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
     location = models.CharField(max_length = 45)
-    description = models.CharField(max_length = 500)
+    description = models.CharField(max_length = 600)
     min_attendees = models.IntegerField()
     max_attendees = models.IntegerField()
-"""class Attendees(models.Model):
-class Users(models.Model):
-class Members(models.Model):
-class Groups(models.Model):"""
+class User(models.Model):
+    is_server_admin = models.BooleanField()
+    dob = models.DateField()
+    email = models.CharField(max_length = 45)
+    irc_user = models.CharField(max_length = 45)
+    hashed_password = models.BinaryField(64)
+class Attendee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+class Member(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    is_group_admin = models.BooleanField()
