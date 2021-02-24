5c67e350923152e3c578a1a1cb33907c47402fef
Kai203 <kps203@exeter.ac.uk>
2021-02-24 23:18:35 +0100

    Add register page with sha3-512 hashing

--- static/exehubapp/javascript/register.js
+++ static/exehubapp/javascript/register.js
@@ -1,12 +1,6 @@
 <!-- Created By Kai -->
 $(document).ready(function(){
     $('#register').submit(function (e) {
-        var password = document.forms["register"]["password"].value;
-        alert(password);
-        if (password == "") {
-            alert("Password must be filled out");
-            return false;
-        }
         alert("Sending...")
         e.preventDefault();
         $.ajax({
